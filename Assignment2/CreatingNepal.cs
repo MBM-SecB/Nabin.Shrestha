a2010ca7d499f25e5802583d26a4b02dcbb8bde8
NabinShrestha <shresthanabin300@gmail.com>
2020-09-28 11:13:16 +0545

    Some Changes in Assignment 2

--- Assignment2/CreatingNepal.cs
+++ Assignment2/CreatingNepal.cs
@@ -54,6 +54,33 @@ class CreatingNepal
         Directory.CreateDirectory(dir5);
         File.WriteAllText(Path.Combine(dir5, "test.txt"), "Chitwan");
 
+         var dir6 = @"E:\Computer Science\.Net Centric Computing\Lab\Nabin.Shrestha\Assignment2\Nepal\Jhapa";
+        if(!Directory.Exists(dir6))
+        Directory.CreateDirectory(dir6);
+        File.WriteAllText(Path.Combine(dir6, "test.txt"), "Jhapa");
+
+         var dir7 = @"E:\Computer Science\.Net Centric Computing\Lab\Nabin.Shrestha\Assignment2\Nepal\Gorkha";
+        if(!Directory.Exists(dir7))
+        Directory.CreateDirectory(dir7);
+        File.WriteAllText(Path.Combine(dir7, "test.txt"), "Gorkha");
+
+         var dir8 = @"E:\Computer Science\.Net Centric Computing\Lab\Nabin.Shrestha\Assignment2\Nepal\Ilam";
+        if(!Directory.Exists(dir8))
+        Directory.CreateDirectory(dir8);
+        File.WriteAllText(Path.Combine(dir8, "test.txt"), "Ilam");
+
+         var dir9 = @"E:\Computer Science\.Net Centric Computing\Lab\Nabin.Shrestha\Assignment2\Nepal\Mustang";
+        if(!Directory.Exists(dir9))
+        Directory.CreateDirectory(dir9);
+        File.WriteAllText(Path.Combine(dir9, "test.txt"), "Mustang");
+
+         var dir10 = @"E:\Computer Science\.Net Centric Computing\Lab\Nabin.Shrestha\Assignment2\Nepal\Sinduli";
+        if(!Directory.Exists(dir10))
+        Directory.CreateDirectory(dir10);
+        File.WriteAllText(Path.Combine(dir10, "test.txt"), "Sinduli");
+
+
+
 
     }    
 
