c348f3a154476e38538366be238d7063d31ec19b
NabinShrestha <shresthanabin300@gmail.com>
2020-09-29 22:12:53 +0545

    Added two assignments of LINQ inside Program.cs

--- LINQ/Country.cs
+++ LINQ/Country.cs
@@ -1,24 +1,24 @@
 using System;
 public class Country
 {
-    public string Name {get; set;}
+    public string Name { get; set; }
 
-    public string Capitol{get; set;}
+    public string Capitol { get; set; }
 
-    public string Continent {get; set;}
-    
-    public long Population {get; set;}
+    public string Continent { get; set; }
 
-    public DateTime IndependenceDay {get; set;}
+    public long Population { get; set; }
+
+    public DateTime IndependenceDay { get; set; }
 
 
-    public Country (string n, string cp, string c, long p, DateTime i = default)
+    public Country(string n, string cp, string c, long p, DateTime i = default)
     {
         Name = n;
         Capitol = cp;
         Continent = c;
         Population = p;
-        IndependencyDay = i;
+        IndependenceDay = i;
     }
 
 
--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -13,46 +13,46 @@ namespace LINQ
 
             int[] numbers = { 11, 22, 32, 13, 53, 65, 76, 34, 21, 65, 342, 33 };
 
-//method syntax
+            //method syntax
             var result1 = numbers.Where(num => num > 50).Select(num => num);
 
             //query expression syntax
 
             var result2 = from num in numbers
-                            where num>50
-                            select num;
+                          where num > 50
+                          select num;
 
 
 
             //restrication - where    
 
             var result3 = from num in numbers
-                            where num>50 && num < 70
-                            select num;      
+                          where num > 50 && num < 70
+                          select num;
 
 
             //projection -select 
             var result4 = from num in numbers
-                            select num*num; 
+                          select num * num;
 
 
-             var result5 = from num in numbers
-                            where num % 2 == 0
-                            select num * num;
+            var result5 = from num in numbers
+                          where num % 2 == 0
+                          select num * num;
 
             //ordering- orderby, orderby desending
 
             var result6 = from num in numbers
-                           //for desending order of even number: where num % 2 == 0
-                            orderby num descending
-                            select num;
+                              //for desending order of even number: where num % 2 == 0
+                          orderby num descending
+                          select num;
 
             //Partitioning: take 
-            var result7 =  numbers.Take(5);//(take 1st 5 numbers)
+            var result7 = numbers.Take(5);//(take 1st 5 numbers)
 
-            var result8 =  numbers.Skip(5).Take(5);//(skip 1st 5 numbers and take 5 number after that)
+            var result8 = numbers.Skip(5).Take(5);//(skip 1st 5 numbers and take 5 number after that)
 
-            
+
             var result9 = numbers.Any(num => num % 2 == 0);//(any element even)
 
             var result10 = numbers.All(num => num % 2 == 0);//(checks if all elements are even)
@@ -60,7 +60,7 @@ namespace LINQ
             var results11 = numbers.Contains(23);//(checks if 23 is in element)
 
             //  Generating:range 
-            var result12 = Enumerable.Range(1,1000);//get 1 to 1000
+            var result12 = Enumerable.Range(1, 1000);//get 1 to 1000
 
             var result13 = Enumerable.Repeat("Hello WOrld", 20); //print hello world 20 times
 
@@ -69,49 +69,62 @@ namespace LINQ
         }
 
 
-    void LearnLinqOnComplexCollection()
-    {
-        List<Country> countries = new List<Country>();
+        void LearnLinqOnComplexCollection()
+        {
+            //List<Country> countries = new List<Country>();
 
-        Country c1 = new Country("Nepal","Kathmandu","Asia",3232323);
-        Country c2 = new Country("India","New Delhi","Asia",3232232333,DateTime.pharse ("1947//1/1"));
-        Country c3 = new Country("US","WashingtonDC","NorthAmerica",323232323);
-        Country c4 = new Country("China","Beijing","Asia",2233232323, DateTime.pharse ("1999/2/3"));
-        Country c5 = new Country("Nepal","Kathmandu","Asia",3232323);
-        Country c6 = new Country("India","New Delhi","Asia",3232232333,DateTime.pharse ("1947//1/1"));
-        Country c7 = new Country("US","WashingtonDC","NorthAmerica",323232323);
-        Country c8 = new Country("China","Beijing","Asia",2233232323, DateTime.pharse ("1999/2/3"));
-        Country c9 = new Country("Nepal","Kathmandu","Asia",3232323);
-        Country c10 = new Country("India","New Delhi","Asia",3232232333,DateTime.pharse ("1947//1/1"));
-        Country c11 = new Country("US","WashingtonDC","NorthAmerica",323232323);
-        Country c12 = new Country("China","Beijing","Asia",2233232323, DateTime.pharse ("1999/2/3"));
-        Country c13 = new Country("Nepal","Kathmandu","Asia",3232323);
-        Country c14 = new Country("India","New Delhi","Asia",3232232333,DateTime.pharse ("1947//1/1"));
-        Country c15 = new Country("US","WashingtonDC","NorthAmerica",323232323);
-        Country c16 = new Country("China","Beijing","Asia",2233232323, DateTime.pharse ("1999/2/3"));
-        Country c17 = new Country("Nepal","Kathmandu","Asia",3232323);
-        Country c18 = new Country("India","New Delhi","Asia",3232232333,DateTime.pharse ("1947//1/1"));
-        Country c19 = new Country("US","WashingtonDC","NorthAmerica",323232323);
-        Country c20 = new Country("China","Beijing","Asia",2233232323, DateTime.pharse ("1999/2/3"));
-
-        //c1.Name="Nepal";
-        //c1.Capitol="Kathmandu";
-        //c1.Continent ="Asia";
-        //c1.Population = "3232323";
-
-        //countries.Add(c1);
-
-        List<Country> countries = new List<Country> {c1,c2,c3,c4};
+            Country c1 = new Country("Nepal", "Kathmandu", "Asia", 3232323);
+            Country c2 = new Country("India", "New Delhi", "Asia", 3232232333, DateTime.Parse("1947/1/1"));
+            Country c3 = new Country("US", "WashingtonDC", "NorthAmerica", 323232323);
+            Country c4 = new Country("China", "Beijing", "Asia", 2233324232323, DateTime.Parse("1999/2/3"));
+            Country c5 = new Country("Bhutan", "A", "Asia", 3232323343);
+            Country c6 = new Country("India1", "New Delhi1", "Asia", 323223233233, DateTime.Parse("1947/1/1"));
+            Country c7 = new Country("Portugal", "Madrid", "Europe", 323232323323);
+            Country c8 = new Country("Spain", "madrid1", "Europe", 223323342323, DateTime.Parse("1888/7/29"));
+            Country c9 = new Country("Pakistal", "Pk1", "Asia", 323232334);
+            Country c10 = new Country("Bangladesh", "Bg1", "Asia", 3232232333433, DateTime.Parse("1945//9/15"));
+            Country c11 = new Country("Germany", "GE1", "Europe", 32323232334);
+            Country c12 = new Country("United Kingdom", "Uk1", "South America", 342233232323, DateTime.Parse("1929/12/23"));
+            Country c13 = new Country("Australia", "Au1", "Australia", 3232323343);
+            Country c14 = new Country("Afganistan", "Af1", "Asia", 32322323343, DateTime.Parse("1908/3/14"));
+            Country c15 = new Country("US", "WashingtonDC", "NorthAmerica", 32433232323);
+            Country c16 = new Country("SouthKorea", "Sk1", "Asia", 223323232433, DateTime.Parse("1997/8/23"));
+            Country c17 = new Country("NorthKorea", "Nk1", "Asia", 3232323453);
+            Country c18 = new Country("Argentina", "Ag2", "Europe", 32454232333, DateTime.Parse("1923/5/13"));
+            Country c19 = new Country("Russia", "Rs2", "NorthAmerica", 3232323);
+            Country c20 = new Country("Netheland", "Nl1", "Europe", 22332323, DateTime.Parse("1932/6/2"));
 
 
-        var resultl9 = from country in countries
-                        where country.Continent == "Asia"
-                        select country.Name;
+            //c1.Name="Nepal";
+            //c1.Capitol="Kathmandu";
+            //c1.Continent ="Asia";
+            //c1.Population = "3232323";
 
-        //      HW - list all asian country which are never been invaded
-        //      HW - list all europian countries in assending order that has population less than 500k          
+            //countries.Add(c1);
+
+            var countries = new List<Country> { c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20 };
+
+
+            var resultl9 = from country in countries
+                           where country.Continent == "Asia"
+                           select country.Name;
+
+            //      HW:1 - list all asian country which are never been invaded
+            //      HW:2 - list all europian countries in assending order that has population less than 500k        
+
+            //hw:1
+            var result23 = from country in countries
+                           where country.IndependenceDay == default
+                           select country.Name;
+
+
+            //hw:2
+            var result21 = from country in countries
+                           orderby country.Population
+                           where country.Continent == "Europe"
+                           select country.Name;
+
+        }
 
     }
-    
-    }
-} 
+}
