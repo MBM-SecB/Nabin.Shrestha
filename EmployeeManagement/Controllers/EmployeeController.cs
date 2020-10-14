3aeddbd43726ee6ce22e118634bc0aac42f68384
NabinShrestha <shresthanabin300@gmail.com>
2020-10-14 10:09:24 +0545

    Added EmployeeDetails

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,38 +1,22 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-
-        List<Employee> employee = new List<Employee>();
-        var employee1 = new Employee
-        {
-            FirstName = "Nabin",
-            SurName = "Shrestha",
-            Address = "NewBaneshwor",
-            Salary = 120000
-        };
-        var employee2 = new Employee
-        {
-            FirstName = "Nabin1",
-            SurName = "Shrestha1",
-            Address = "NewBaneshwor1",
-            Salary = 1200001
-        };
-        var employee3 = new Employee
-        {
-            FirstName = "Nabin2",
-            SurName = "Shrestha2",
-            Address = "NewBaneshwor2",
-            Salary = 1200002
-        };
-        var employees = new List<Employee> { employee1, employee2, employee3 };
+        var employees = Employee.GetEmployees();        
         return View(employees);
     }
 
-
+    public ActionResult Detail(string firstName)
+    {
+        var employees = Employee.GetEmployees();
+        var employee = employees.FirstOrDefault(x => x.FirstName == firstName);
+        
+        return View(employee);
+    }
 }
 public class Employee
 {
@@ -42,5 +26,3 @@ public class Employee
     public char Gender { get; set; } = 'M';
     public double Salary { get; set; }
 }
-
-public
